6d45c9534ef7f25259bdfdf6cabe88575de1708c
Ralf Kaczich <ralf.kaczich@gmail.com>
2021-02-22 11:31:18 +0100

    Vorbereitung zur Darstellung aus LocalStorage

--- js/services/employeeService.js
+++ js/services/employeeService.js
@@ -1,4 +1,39 @@
 class EmployeeService {
+
+  static init(){
+    var myLocalStorage = window.localStorage;
+    console.log(myLocalStorage);
+
+  for (var key in myLocalStorage) {
+    // hole json string aus local storage
+    var employee = myLocalStorage.getItem(key);
+    //console.log(employee);
+    // parse json string to class Emplyee
+    try {
+      var empl = JSON.parse(employee);
+      var newEmployee = new Employee(empl.id, empl.name, empl.email, empl.role);
+      if(newEmployee instanceof Employee){
+        console.log("JO Mitarbeiter wiederhergestellt");
+        // hier gehts weiter......
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  }
+
+
+  static deleteEmployee(event){
+    console.log("Event:"+event.target.id);
+
+    var myLocalStorage = window.localStorage;
+    myLocalStorage.removeItem(event.target.id);
+
+    var idToDelete = event.target.id;
+    var myobj = document.getElementById(idToDelete);
+    myobj.remove();
+  }
+
   static addEmployee(event){
 
     var name = document.getElementById("id_name").value;
@@ -7,29 +42,32 @@ class EmployeeService {
     var id = new Date().getTime();
 
     var employee = new Employee(id, name, email, role);
-
     console.log(employee);
 
-  var alle_zeilen = document.getElementById("alle vorhanden zeilen");
-  alle_zeilen + neue Zeile
-  add als innerHTML
+    var myLocalStorage = window.localStorage;
+    myLocalStorage.setItem(employee.id, JSON.stringify(employee) );
 
-    var row =
-      "<div id="1" class='row'>"+
+   var alle_zeilen = document.getElementById("userContent").innerHTML;
+   var row = "<div id="+ employee.id +" class='row'>"+
       "<div class='col'>" + employee.id + "</div>"+
       "<div class='col'>" + employee.name + "</div>"+
       "<div class='col'>" + employee.email + "</div>"+
       "<div class='col'>" + employee.role + "</div>"+
-      "<div class='col'>" +  + "</div>"+
+      "<div class='col'><button class='button' onClick='EmployeeService.deleteEmployee(event)'><i id="+employee.id+" class='fa fa-trash'></i></button>   </div>"+
       "</div>"
 
+      console.log(document);
+   alle_zeilen = alle_zeilen + row;
+   document.getElementById("userContent").innerHTML = alle_zeilen;
+
+/**
     var div = document.createElement("div");
     div.setAttribute("class", "row");
 
     var col = document.createElement("div");
     col.setAttribute("class", "col");
     col.innerHTML = employee.id;
-    div.appendChild(col);
+      div.appendChild(col);
 
     var col2 = document.createElement("div");
     col2.setAttribute("class", "col");
@@ -53,7 +91,7 @@ class EmployeeService {
 
     var table = document.getElementById("table");
     table.appendChild(div);
-
+**/
     //document.body.insertBefore(document.getElementById("userContent"), div);
     //document.getElementById("userContent").innerHTML = row;
   }
